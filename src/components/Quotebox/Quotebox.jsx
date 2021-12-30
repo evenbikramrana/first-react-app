4002f5284b5dd85db50535a543fc3445c670a9e1
Bikram <ranabikram49@gmail.com>
2021-12-31 00:55:44 +0530

    Added animation

--- src/components/Quotebox/Quotebox.jsx
+++ src/components/Quotebox/Quotebox.jsx
@@ -1,44 +1,71 @@
-import React, { useState } from 'react';
-import Button from 'components/Button';
-import './quotebox.css';
-import { motion } from 'framer-motion';
-
+import React, { useState, useEffect } from "react";
+import Button from "components/Button";
+import "./quotebox.css";
+import { motion, useAnimation } from "framer-motion";
 
 const Quotebox = () => {
-    const [quote, setQuote] = useState('');
-    const [author, setAuthor] = useState('');
-    const handleGetQuote = () => {
-        console.log('get quote');
-        fetch('https://quotes15.p.rapidapi.com/quotes/random/', {
-            method: 'GET',
-            headers: {
-                'x-rapidapi-host': 'quotes15.p.rapidapi.com',
-                'x-rapidapi-key': '1ab2a061c6msh28bab6680d0f6dbp1df1b9jsndb2f73fb9922'
-            },
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            setQuote(data.content);
-            setAuthor(data.originator.name);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const controls = useAnimation();
+  const handleGetQuote = () => {
+    console.log("get quote");
+    fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": "1ab2a061c6msh28bab6680d0f6dbp1df1b9jsndb2f73fb9922",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        controls.start({
+          x: [-300, 0],
+          opacity: [0, 1],
+          transition: {
+            duration: 1,
+            ease: "easeInOut",
+            staggerChildren: .4,
 
-        }).catch(err => {
-            console.log(err);
+          },
+
         });
+        setQuote(data.content);
+        setAuthor(data.originator.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    handleGetQuote();
+    console.log("use effect");
+  }, []);
+  return (
+    <div className="quote-box">
+      <h3 className="quote-box__header">Generate Random Quote</h3>
+      <div className="overflow-hidden">
+        <motion.div animate={controls} initial={{opacity:0,x:-300}} className="quote-box__content">
+          {quote ? (
+            <motion.p animate={controls} className="quote-box__quote">{quote}</motion.p>
+          ) : (
+            <p className="quote-box__quote">Quote will be displayed here</p>
+          )}
+          {author ? (
+            <p className="quote-box__author">{author}</p>
+          ) : (
+            <p className="quote-box__author">Author will be displayed here</p>
+          )}
+        </motion.div>
+      </div>
 
-    }
-    return (
-        <div className='quote-box'>
-            <h3 className='quote-box__header'>Generate Random Quote</h3>
-            <motion.div className="quote-box__content">
-                {quote ? <p className='quote-box__quote'>{quote}</p> : <p className='quote-box__quote'>Quote will be displayed here</p>}
-                {author ? <p className='quote-box__author'>{author}</p> : <p className='quote-box__author'>Author will be displayed here</p>}
-            </motion.div>
-            <div className="quote-box__footer">
-                <Button type="primary" text="Get Quote" onClick={handleGetQuote} />
-                <Button type="secondary" text="Tweet Quote" />
-            </div>
-        </div>
-    )
-}
+      <div className="quote-box__footer">
+        <Button type="primary" text="Get Quote" onClick={handleGetQuote} />
+        <Button type="secondary" text="Tweet Quote" />
+      </div>
+    </div>
+  );
+};
 
-export default Quotebox
+export default Quotebox;
